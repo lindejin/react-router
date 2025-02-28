6d0dc0b2bc62bed7c67bf99ca38bce0f67a0f7f0
yazu <1223042416@qq.com>
2025-03-01 00:51:41 +0800

    Install and configure shadcn/ui for React Router.

---
+++ app/routes/home.tsx
@@ -0,0 +1,31 @@
+// import type { Route } from "./+types/home";
+// import { Welcome } from "../welcome/welcome";
+
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: "New React Router App" },
+//     { name: "description", content: "Welcome to React Router!" },
+//   ];
+// }
+
+// export default function Home() {
+//   return <Welcome />;
+// }
+import { Button } from "~/components/ui/button"
+
+import type { Route } from "./+types/home"
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ]
+}
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  )
+}
